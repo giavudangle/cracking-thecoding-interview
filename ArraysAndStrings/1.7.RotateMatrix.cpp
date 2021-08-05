654a9e7c651fb242acad7db8d2eccc4be4ae6eee
Vudang <danglegiavu@gmail.com>
2021-08-05 16:43:12 +0700

    update

---
+++ ArraysAndStrings/1.7.RotateMatrix.cpp
@@ -0,0 +1,14 @@
+/* 1.7.Rotate Matrix : Given an image represented by an NxN
+where each pixels in the image is 4bytes.Wriate a method to
+rotate the image by 90 degrees.
+*/
+
+#include <bits/stdc++.h>
+#include "MyHashTable.h"
+#include "MyLinkedList.h"
+
+int main(){
+	
+	return 1;
+}
+
---
+++ ArraysAndStrings/1.8.ZeroMatrix.cpp
@@ -0,0 +1,14 @@
+/* 1.9.String Rotation : Assume you have a method isSubString which checks if one word is a substring of another. 
+Given two strings, s1 and s2, write code to check if s2 is a roration of s1 using only one call to isSubString
+*/
+
+
+#include <bits/stdc++.h>
+#include "MyHashTable.h"
+#include "MyLinkedList.h"
+
+int main(){
+	
+	return 1;
+}
+
