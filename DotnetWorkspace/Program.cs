55cd5d9edb1393f45e55c1440472a7c95050ae8e
Vudang <danglegiavu@gmail.com>
2021-08-20 12:46:00 +0700

    update workspaces

---
+++ DotnetWorkspace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetWorkspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
